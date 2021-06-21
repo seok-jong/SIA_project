91f4bf929c17d60d3eacf312aa67180e602d5b4e
ceuity <everland7942@gmail.com>
2021-06-21 18:01:55 +0900

    Add: files to deploy

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,16 @@ FROM tensorflow/tensorflow:latest-gpu
 RUN apt-get update
 RUN apt-get install -y libgl1-mesa-dev wget vim
 
-RUN pip install keras numpy pillow flask gunicorn
+RUN pip install keras numpy pillow flask gunicorn opencv-python segmentation_models albumentations
 
 WORKDIR root
 COPY ./app.py ./app.py
-COPY ./auto_painter.py ./auto_painter.py
-COPY ./image_preprocessor.py ./image_preprocessor.py
 COPY ./templates/index.html ./templates/index.html
 COPY ./images/sample_image.png ./images/sample_image.png
 
 RUN mkdir model
-RUN wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=10z10M6qV8KPv5XCOtdkhIE1x9pzgBcvs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=10z10M6qV8KPv5XCOtdkhIE1x9pzgBcvs" -O auto_painter_model.h5 && rm -rf /tmp/cookies.txt
-RUN mv auto_painter_model.h5 model/
+
+COPY ./model/mixed_928.h5 ./model/mixed_928.h5
 
 EXPOSE 5000
 ENTRYPOINT ["python"]
---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, request, Response, send_from_directory
+
+import os
+import datetime
+import re
+from io import BytesIO
+
+import tensorflow as tf
+import keras
+from PIL import Image
+import numpy as np
+import cv2
+
+os.environ['SM_FRAMEWORK'] = 'tf.keras'
+import segmentation_models as sm
+
+
+# If you want to use GPU, Comment out this line.
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+config = ({"input_size": 1024,
+           "image_size": 864,
+           "backbone": "efficientnetb7",
+           "method": "unet",
+           "class": "mixed"
+          })
+
+CLASSES = ['building', 'roads']
+orig_path = './origin_images/'
+result_path = './result_images/'
+
+if not os.path.exists(orig_path):
+    os.mkdir(orig_path)
+if not os.path.exists(result_path):
+    os.mkdir(result_path)
+
+import albumentations as A
+
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+    
+    Args:
+        preprocessing_fn (callbale): data normalization function 
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+    
+    """
+    
+    _transform = [
+        A.Lambda(image=preprocessing_fn),
+        A.Resize(config['image_size'], config['image_size'])
+    ]
+    return A.Compose(_transform)
+
+# helper function for data visualization    
+def denormalize(x):
+    """Scale image to range 0..1 for correct plot"""
+    x_max = np.percentile(x, 98)
+    x_min = np.percentile(x, 2)    
+    x = (x - x_min) / (x_max - x_min)
+    x = x.clip(0, 1)
+    x *= 255
+    return x
+
+# 전처리
+BACKBONE = config['backbone']
+preprocess_input = sm.get_preprocessing(BACKBONE)
+preprocessing = get_preprocessing(preprocess_input)
+
+# define network parameters
+n_classes = 1 if len(CLASSES) == 1 else (len(CLASSES) + 1)  # case for binary and multiclass segmentation
+activation = 'sigmoid' if n_classes == 1 else 'softmax'
+
+#create model
+model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
+
+# 불러올 모델 파일 위치
+model.load_weights('./model/mixed_928.h5')
+
+app = Flask(__name__, template_folder="./templates/",
+            static_url_path="/images", static_folder="images")
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/healthz", methods=['GET'])
+def healthCheck():
+    return "", 200
+
+
+@app.route("/images", methods=['POST'])
+def get_result():
+    if request.method == "POST":
+        width, height = 512, 512
+        try:
+            curr_time = datetime.datetime.now()
+            curr_time = re.sub(r'[ :]','-', str(curr_time))
+            source = request.files['source'].read()
+            orig = Image.open(BytesIO(source))
+            orig.save(f'{orig_path}{curr_time}_orig.png', 'png')
+            image = cv2.imread(f'{orig_path}{curr_time}_orig.png')
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            preprocessed = preprocessing(image=image)
+            image = preprocessed['image']
+            image = np.expand_dims(image, axis=0)
+            pr_mask = model.predict(image).round()
+            output = denormalize(np.squeeze(pr_mask, axis=0))
+            output = cv2.resize(output, (config['input_size'], config['input_size']))
+            filename = f'{curr_time}.png'
+            cv2.imwrite(result_path + filename, output)
+            
+            return send_from_directory(result_path, filename, as_attachment=True)
+
+        except Exception as e:
+            print("error : %s" % e)
+            return Response("fail", status=400)
+
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='80')
